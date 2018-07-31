604e68306f5bf5ffd75de646e0eebd74aa9677d5
Danilo Pagano <dmpeletronic@gmail.com>
2018-07-31 00:15:44 -0300

    Add more images. Improve README. Add opencv script to preprocess the image.

--- README.md
+++ README.md
@@ -1,21 +1,24 @@
 # proccup
-Cupon Proccessing
+# Cupon Proccessing
 
-Dependencies:
+# Dependencies:
 
-Tesseract
+## Tesseract
 https://github.com/tesseract-ocr/tesseract
 https://pypi.org/project/pytesseract/
 https://blog.codeexpertslearning.com.br/lendo-imagens-uma-abordagem-%C3%A0-ocr-com-google-tesseract-e-python-ee8e8009f2ab
 sudo pip install pytesseract
 sudo apt-get install tesseract-ocr tesseract-ocr-por
 
-Numpy
+## Numpy
 sudo pip install numpy
 
-Pillow
+## Pillow
 sudo pip install pillow
 
-OpenCv
+## OpenCv
 sudo pip install opencv-python
 
+## MatPlotLib
+sudo pip install matplotlib
+sudo apt-get install python-tk
--- python/img_to_txt_tesseract_cv.py
+++ python/img_to_txt_tesseract_cv.py
@@ -2,37 +2,55 @@ import sys as sys
 import pytesseract as ocr 
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 from PIL import Image
+
+
+def showimg(img, texto):
+	cv.imshow(texto, img)
+	#cv.waitKey(0)
+   #plt.imshow(img),plt.title(texto)
+   #plt.xticks([]), plt.yticks([])
+   #plt.show()
+
+def img_to_text(img):
+  # Write the image to disk
+  filename = "{}.png".format(os.getpid())
+  cv.imwrite(filename, img)
+  phrase = ocr.image_to_string(Image.open(filename), lang='por')
+  os.remove(filename)	
+  print(phrase)
+  return phrase
+
 
 
 path=sys.argv[1]
 print "File from path ", path
 
 # Preparando a imagem
-img = Image.open(path).convert('RGB')
+img = cv.imread(path)#Image.open(path).convert('RGB')
+showimg(img, "original")
+img_to_text(img)
 
-# Convertendo em array editavel numpy [x, y, CANALS]
-npimagem = np.asarray(img).astype(np.uint8)  
-
-# diminuicao dos ruidos antes da binarizacao
-npimagem[:, :, 0] = 0 # zerando o canal R (RED)
-npimagem[:, :, 2] = 0 # zerando o canal B (BLUE)
 
 # atribuicao em escala de cinza
-im = cv.cvtColor(npimagem, cv.COLOR_RGB2GRAY) 
+grayscale = cv.cvtColor(img, cv.COLOR_RGB2GRAY) 
+showimg(grayscale, "gray")
+img_to_text(grayscale)
+
+#Suavizando
+filtro = cv.medianBlur(grayscale,3)
+showimg(filtro, "filtro")
+img_to_text(filtro)
 
 # aplicacao da truncagem binaria para a intensidade
 # pixels de intensidade de cor abaixo de 127 serao convertidos para 0 (PRETO)
 # pixels de intensidade de cor acima de 127 serao convertidos para 255 (BRANCO)
 # A atrubicao do THRESH_OTSU incrementa uma analise inteligente dos nivels de truncagem
-ret, thresh = cv.threshold(im, 127, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+ret, thresh = cv.threshold(filtro, 127, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+showimg(thresh, "binaria")
 
-# reconvertendo o retorno do threshold em um objeto do tipo PIL.Image
-binimg= Image.fromarray(thresh) 
-
-phrase = ocr.image_to_string(binimg, lang='por')
-
-print(phrase)
-
-
+cv.waitKey(0)
+cv.destroyAllWindows()
